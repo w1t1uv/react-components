fe02494d7f7a291dc5a429b6fad9bb13eaeab1b4
Egor Ushakov <witluv@icloud.com>
2023-11-04 22:49:15 +0300

    feat: add routing to the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { PokemonSearch } from './PokemonSearch';
+import { Routes, Route } from 'react-router-dom';
+import PageNotFound from './PageNotFound';
 
 function App() {
   return (
     <div className="container">
-      <PokemonSearch />
+      <Routes>
+        <Route path="/" element={<PokemonSearch />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </div>
   );
 }
