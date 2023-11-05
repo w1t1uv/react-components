6e6c761b9dacc569b565f0617b85812253782780
Egor Ushakov <witluv@icloud.com>
2023-11-05 23:16:37 +0300

    feat: add a jump to the details page when clicking on a pokemon card

--- src/AllPokemon.tsx
+++ src/AllPokemon.tsx
@@ -1,8 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { Pokemon } from './Pokemon';
 import { urlObject } from './server';
+
+interface pokemonData {
+  name: string;
+  height: number;
+  isDefault: boolean;
+  order: number;
+  weight: number;
+}
+
+interface responsePokemon {
+  name: string;
+  url: string;
+}
 
 function AllPokemon() {
   const location = useLocation();
@@ -11,14 +24,25 @@ function AllPokemon() {
   const navigate = useNavigate();
   const url = urlObject.url;
 
-  const [pokemonData, setPokemonData] = useState([]);
+  const [allPokemonData, setAllPokemonData] = useState<pokemonData[]>([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       try {
         const response = await axios.get(`${url}?offset=${(page - 1) * 10}&limit=10`);
-        setPokemonData(response.data.results);
+
+        const allPokemonData = await Promise.all(
+          response.data.results.map(async (pokemon: responsePokemon) => {
+            const pokemonResponse = await fetch(pokemon.url);
+            if (!pokemonResponse.ok) {
+              throw new Error(`Query execution error : ${pokemonResponse.status}`);
+            }
+            return pokemonResponse.json();
+          })
+        );
+
+        setAllPokemonData(allPokemonData);
         console.log(response.data.results);
         setLoading(false);
       } catch (e) {
@@ -36,18 +60,20 @@ function AllPokemon() {
   };
 
   return (
-    <div>
+    <div className="cards">
       {loading && <p className="loading">Loading</p>}
       {!loading &&
-        pokemonData.map((pokemon) => (
-          <Pokemon
-            key={pokemon.name}
-            name={pokemon.name}
-            height={pokemon.height}
-            isDefault={pokemon.isDefault ? 'Yes' : 'No'}
-            order={pokemon.order}
-            weight={pokemon.weight}
-          />
+        allPokemonData.map((pokemon) => (
+          <Link key={pokemon.name} to={`/details/${pokemon.name}`} className="pokemon-card">
+            <Pokemon
+              key={pokemon.name}
+              name={pokemon.name}
+              height={pokemon.height}
+              isDefault={pokemon.isDefault ? 'Yes' : 'No'}
+              order={pokemon.order}
+              weight={pokemon.weight}
+            />
+          </Link>
         ))}
       <div className="wrapper">
         <button
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { PokemonSearch } from './PokemonSearch';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 import PageNotFound from './PageNotFound';
+import Details from './Details';
 
 function App() {
   return (
     <div className="container">
       <Routes>
         <Route path="/" element={<PokemonSearch />} />
+        <Route path="/details/:name" element={<Details />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </div>
