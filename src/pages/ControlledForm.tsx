3e0e20ae4566185d76564148e40c75a29e864bae
Egor Ushakov <witluv@icloud.com>
2023-12-02 17:50:54 +0300

    feat: improve the form submission function

--- src/pages/ControlledForm.tsx
+++ src/pages/ControlledForm.tsx
@@ -2,42 +2,62 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 import { IForm } from '../types';
 import { useDispatch, useSelector } from 'react-redux';
 import { setFormData } from '../store/formsSlice';
+import { userSchema } from '../validations/userValidation';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useNavigate } from 'react-router-dom';
+import { convert2base64 } from '../services/convert2base64';
 
 const ControlledForm = () => {
-  const { register, handleSubmit } = useForm<IForm>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>({
     defaultValues: {},
+    resolver: yupResolver(userSchema),
   });
   const dispatch = useDispatch();
   const countries = useSelector((state) => state.countries.countries);
+  const navigate = useNavigate();
 
-  const submit: SubmitHandler<IForm> = (data) => {
+  const submit: SubmitHandler<IForm> = async (data) => {
     console.log(data);
+    if (data.image) {
+      const converted = await convert2base64(data.image[0] as File);
+      data.image = converted;
+    }
     dispatch(setFormData(data));
+    navigate(`/`);
   };
 
   return (
-    <div>
-      <h1>Controlled Form</h1>
+    <div className="form-wrapper">
+      <p className="title">Controlled Form</p>
       <form onSubmit={handleSubmit(submit)} className="form">
         <div className="name">
           <label htmlFor="name">Name</label>
           <input {...register('name')} />
+          <p>{errors.name?.message}</p>
         </div>
         <div className="age">
           <label htmlFor="age">Age</label>
           <input type="number" {...register('age')} />
+          <p>{errors.age?.message}</p>
         </div>
         <div className="email">
           <label htmlFor="email">Email</label>
           <input {...register('email')} />
+          <p>{errors.email?.message}</p>
         </div>
         <div className="password">
           <label htmlFor="password">Password</label>
           <input {...register('password')} />
+          <p>{errors.password?.message}</p>
         </div>
         <div className="confirmedPassword">
           <label htmlFor="confirmedPassword">Confirm password</label>
           <input {...register('confirmedPassword')} />
+          <p>{errors.confirmedPassword?.message}</p>
         </div>
         <div className="gender">
           <label htmlFor="gender">Gender</label>
@@ -50,10 +70,12 @@ const ControlledForm = () => {
         <div className="termsAndConditions">
           <label htmlFor="termsAndConditions">Accept the T&C</label>
           <input type="checkbox" {...register('termsAndConditions')} />
+          <p>{errors.termsAndConditions?.message}</p>
         </div>
-        <div className="image">
+        <div className="upload-image">
           <label htmlFor="image">Upload image</label>
           <input type="file" name="image" {...register('image')} />
+          <p>{errors.image?.message}</p>
         </div>
         <div className="countries">
           <label htmlFor="country">Choose the country</label>
@@ -65,7 +87,7 @@ const ControlledForm = () => {
             ))}
           </select>
         </div>
-        <button>Submit</button>
+        <button className="submit">Submit</button>
       </form>
     </div>
   );
