c6c36c35ec92792303e91706a7db7b01b8f59370
Egor Ushakov <witluv@icloud.com>
2023-12-02 17:47:38 +0300

    feat: add convert to base64

---
+++ src/services/convert2base64.ts
@@ -0,0 +1,14 @@
+export const convert2base64 = (file: File): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onload = () => {
+      resolve(reader.result as string);
+    };
+
+    reader.onerror = (error) => {
+      reject(error);
+    };
+  });
+};
